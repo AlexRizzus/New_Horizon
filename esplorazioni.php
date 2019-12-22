dec6859af94c70001e51c76bba9587af60a9421e
Cis8 <ema.cis8@gmail.com>
2019-12-22 17:10:51 +0100

    altri bug fixes (e non sono gli ultimi)

--- esplorazioni.php
+++ esplorazioni.php
@@ -5,25 +5,25 @@ $connessioneOK=$oggettoConnessione->openDBConnection();
 $paginaHTML = file_get_contents('esplorazioni.html');
 
 if($connessioneOK){
-  $missioni = $oggettoConnessione->getMissioni_perLuogo("Marte"); //parametro dal form
-  if(mysqli_num_rows($queryResult)==0){
+  $missioni = $oggettoConnessione->getMissioni_perLuogo("'Marte'"); //parametro dal form
+  if(mysqli_num_rows($missioni)==0){
     echo("risultato query vuoto");
-  }else{
-    $stringa_missioni = "";
-    foreach($missioni as $valore){
-      $stringa_missioni .= "<div class="mission-box">" .
-      "<h2>Nome della missione: " . $valore['Nome'] . "</h2>" .
-      "<p>Iniziata in data: " . $valore['data_inizio'] . "</p>" .
-      "<p>Fine in data: " . $valore['data_fine'] . "</p>" .
-      "<p>Stato: " . $valore['stato'] . "</p>" .
-      "<p>Affiliazioni: " . $valore['affiliazioni'] . "</p>" .
-      "<p>Luogo: " . $valore['destinazione'] . "</p>" .
-      "<p>Scopo: " . $valore['scopo'] . "</p>" .
-      "</div>";
+  } else {
+      $stringa_missioni = "";
+      foreach($missioni as $valore){
+        $stringa_missioni .= '<div class="mission-box">' .
+        "<h2>Nome della missione: " . $valore['nome'] . "</h2>" .
+        "<p>Iniziata in data: " . $valore['data_inizio'] . "</p>" .
+        "<p>Fine in data: " . $valore['data_fine'] . "</p>" .
+        "<p>Stato: " . $valore['stato'] . "</p>" .
+        "<p>Affiliazioni: " . $valore['affiliazioni'] . "</p>" .
+        "<p>Luogo: " . $valore['destinazione'] . "</p>" .
+        "<p>Scopo: " . $valore['scopo'] . "</p>" .
+        "</div>";
+      }
+      echo str_replace("<missionsHere/>", $stringa_missioni, $paginaHTML);
     }
-    echo str_replace("<missionsHere/>", $stringa_missioni, $paginaHTML);
-  }
-  else
+  }else
   {
     echo("connessione fallita come rizzo" );
   }
