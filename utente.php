494086a55f69950f06a154c8d36b699134c3b870
Alessandro Rizzo <alex.rizzo1998@gmail.com>
2020-01-24 11:43:40 +0100

    aggiungi php utente

---
+++ utente.php
@@ -0,0 +1,25 @@
+<?php
+  require_once("dbConnection.php");
+  $oggettoConnessione=new DBAccess();
+  $connessioneOK=$oggettoConnessione->openDBConnection();
+  $paginaHTML = file_get_contents('utente.html');
+  if(isset($_SESSION['livello']))
+  {
+    if( $_SESSION['livello'] == 'generico')
+    {
+      $elencopref = "";
+      $query= "SELECT nome FROM Utenti_Missioni WHERE username = '" . $_SESSION['username'] ."'";
+      $result_pref = mysqli_query($oggettoConnessione->connection, $query );
+      $query= "SELECT nome FROM Utenti_Missioni WHERE username = '" . $_SESSION['username'] ."'";
+      $result = mysqli_query($oggettoConnessione->connection, $query );
+      
+    }
+    else {
+      // non è un utente, ridireziona alla pagina admin
+      header('admin.php');
+    }
+  }
+  else {
+    // non è autenticato, ridireziona alla pagina login
+    header('login.php');
+  }
