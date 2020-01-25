a7eb913a0835fbf65537884e0f15aff86bf52351
DAVIDE MARIA LAZZARO <davidemaria.lazzaro@studenti.unipd.it>
2020-01-25 18:10:04 +0100

    Aggiunto controllo input modifica missione #19

--- dbConnection.php
+++ dbConnection.php
@@ -162,6 +162,11 @@ public function getNomiMissioniPreferite($username) {
   }
   return $result;
 }
-
+public function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
 }
 ?>
