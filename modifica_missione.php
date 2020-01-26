6127c94a8511d18d5031b5763128445ce678e05e
DAVIDE MARIA LAZZARO <davidemaria.lazzaro@studenti.unipd.it>
2020-01-26 16:29:33 +0100

    modifica missione scrittura database #19

--- modifica_missione.php
+++ modifica_missione.php
@@ -8,6 +8,7 @@ session_start();
 if(isset($_SESSION['livello'])){
     if($_SESSION['livello'] == "amministratore"){
         //creazione form modifica
+        
         if (isset($_POST['updateMission']))
         {
             $missione = $_POST['nome'];
@@ -19,21 +20,34 @@ if(isset($_SESSION['livello'])){
             Stato: <input type="text" name="stato" value="'. $missioni[0]['stato'] .'"></Stato><br>
             Affiliazioni: <input type="text" name="affiliazioni" value="'. $missioni[0]['affiliazioni'] .'"></Affiliazioni><br>
             Luogo: <input type="text" name="destinazione" value="'. $missioni[0]['destinazione'] .'"></Luogo><br>
-            Scopo: <input type="text" name="scopo" value="'. $missioni[0]['scopo'] .'"></Scopo></fieldset><br><br><input type="submit" value="Invia" name="change">';
+            Scopo: <input type="text" name="scopo" value="'. $missioni[0]['scopo'] .'"></Scopo></fieldset><br><br><input type="hidden" value="'.$missione.'" name="key"><input type="submit" value="Invia" name="change">';
             $paginaHTML=str_replace("</campiDati>",$stringa, $paginaHTML);
             echo($paginaHTML);
-        }
+            
         
-
+        }
         if (isset($_POST['change'])) {
             $nome= $data_inizio =$data_fine=$stato=$affiliazioni=$destinazione=$scopo= "";
+            $missione=$_POST['key'];
+            $nome=$oggettoConnessione->test_input($_POST["nome"]);
             $data_inizio = $oggettoConnessione->test_input($_POST["data_inizio"]);
             $data_fine = $oggettoConnessione->test_input($_POST["data_fine"]);
             $stato = $oggettoConnessione->test_input($_POST["stato"]);
             $affiliazioni = $oggettoConnessione->test_input($_POST["affiliazioni"]);
             $destinazione = $oggettoConnessione->test_input($_POST["destinazione"]);
             $scopo = $oggettoConnessione->test_input($_POST["scopo"]);
-            $nome = $oggettoConnessione->test_input($_POST["nome"]);
+
+            
+            $oggettoConnessione->modifica_nome($missione, $nome);
+            $oggettoConnessione->modifica_data_inizio($missione, $data_inizio);
+            $oggettoConnessione->modifica_data_fine($missione, $data_fine);
+            $oggettoConnessione->modifica_stato($missione, $stato);
+            $oggettoConnessione->modifica_affiliazioni($missione, $affiliazioni);
+            $oggettoConnessione->modifica_destinazione($missione, $destinazione);
+            $oggettoConnessione->modifica_scopo($missione, $scopo);
+            
+
+
             $stringa2='<p>Modifiche inviate</p>';
             $stringa3='<p>Puoi tornare alla selezione missioni</p>';
             $paginaHTML = file_get_contents('modifica_missione.html');
