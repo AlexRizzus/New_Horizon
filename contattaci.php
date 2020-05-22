c3fb8adaf62f641fc563796a10dd825d2eee8400
dlazzaro <dmlazzaro98@gmail.com>
2020-05-22 10:33:01 +0200

    #21 modificato php contattaci cis8 super gay

--- contattaci.php
+++ contattaci.php
@@ -9,10 +9,12 @@ $stringa='<form id="formid" class="form" action="contattaci.php" method="post"><
         <div/><datalist id="opzioni"><option value="Richiesta Informazioni"/><option value="Segnalazione di un problema"/><option value="Richiesta di collaborazione"/>
         <option value="Una richiesta scientifica"/><option value="Altro"/></datalist><br/><textarea tabindex="6" name="testo" rows="10" cols="100" placeholder="Scrivi qui il tuo messaggio."></textarea>
         <div><input tabindex="7" type="submit" value="Invia" name="invio"></div></form>';
-$paginaHTML=str_replace("<sostituto></sostituto>",$stringa, $paginaHTML);
+$paginaHTML=str_replace("<sostituto id='formid'></sostituto>",$stringa, $paginaHTML);
 echo($paginaHTML);
-if(isset($_POST['invio']))
+$testo = "";
+if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['invio']))
 {
+    $testo = test_input($_POST["testo"]);
     if(isset($_SESSION['email'])){
 
         $headers = 'From: '.$_SESSION['email'].'';
@@ -20,4 +22,10 @@ if(isset($_POST['invio']))
         mail("newhorizon@gmail.com",$_POST['opzioni'], $_POST['testo'], $headers);
     }
 }
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+  }
 ?>
