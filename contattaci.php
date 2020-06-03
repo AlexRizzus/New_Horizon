9d4fb757c7e737cd0d529b5ef7245cbb009780c6
dlazzaro <dmlazzaro98@gmail.com>
2020-06-03 11:56:01 +0200

    sistemata accessibilita pagina contattaci

--- contattaci.php
+++ contattaci.php
@@ -6,11 +6,11 @@ $paginaHTML = file_get_contents('contattaci.html');
 session_start();
 
 $stringa='<form id="formid" class="form" action="contattaci.php" method="post"><p class="errorEmail"></p>
-<div><p class = "formdescription">Inserisci qui l&#8217;oggetto del tuo messaggio</p><input class="inputform" type="text" tabindex="13" name="opzioni"/>
+<div><p class = "formdescription">Inserisci qui l&#8217;oggetto del tuo messaggio</p><label class = "formdescription" for="oggetto">Oggetto del messaggio: </label><input id="oggetto" class="inputform" type="text" tabindex="10" name="opzioni"/>
 <p class="errorOggetto"></p></div>
-<div><p class = "formdescription">Inserisci qua sotto tutto quello che vuoi comunicarci</p><textarea class="inputform" tabindex="14" id="textarea" name="testo" rows="7" cols="200"></textarea></div>
+<div><label class = "formdescription" for="textarea">Scrivi qua il messaggio: </label><textarea class="inputform" tabindex="11" id="textarea" name="testo" rows="7" cols="200"></textarea></div>
 <p class="errorTextArea"></p>
-<div><input tabindex="15" type="submit" value="Invia" name="invio" id="sendbutton"/></div></form>';
+<div><label class = "formdescription" for="sendbutton">Invia il messagio al nostro staff: </label><input tabindex="12" type="submit" value="Invia" name="invio" id="sendbutton"/></div></form>';
 $paginaHTML=str_replace('<p id="sostituto"></p>',$stringa, $paginaHTML);
 
 $testo = "";
@@ -21,18 +21,18 @@ $reqEmail="";
 if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['invio']))
 {
     if(!(isset($_SESSION['email']))){
-        $reqEmail= '<p class="errorEmail">Devi fare il login per poter inviare la richiesta</p>';
+        $reqEmail= '<p class = "formerror">Devi fare il login per poter inviare la richiesta</p>';
         $paginaHTML=str_replace('<p class="errorEmail"></p>',$reqEmail, $paginaHTML);   
     }
     if(empty($_POST["testo"])){
-        $reqTesto = '<p class="errorTextArea">Il testo non pu&ograve; essere vuoto</p>';
+        $reqTesto = '<p class = "formerror">Il testo non pu&ograve; essere vuoto</p>';
         $paginaHTML=str_replace('<p class="errorTextArea"></p>',$reqTesto, $paginaHTML);
     }
     else{
         $testo="true";
     }
     if(empty($_POST["opzioni"])){
-        $reqOggetto= '<p class="errorOggetto">Inserire un oggetto</p>';
+        $reqOggetto= '<p class = "formerror">Inserire un oggetto</p>';
         $paginaHTML=str_replace('<p class="errorOggetto"></p>',$reqOggetto, $paginaHTML);
     }
     else{
